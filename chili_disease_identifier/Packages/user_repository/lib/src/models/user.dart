e715c1be95490b8648f89cefe20804a056891b3b
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2025-03-16 00:00:08 +0530

    feat:firebase configurations

---
+++ chili_disease_identifier/Packages/user_repository/lib/src/models/user.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+import '../entities/entities.dart';
+
+
+
+class MyUser extends Equatable{
+  final String userId;
+  final String email;
+  final String name;
+
+  const MyUser({
+   required this.userId,
+   required this.email,
+   required this.name
+});
+
+  MyUser copyWith({String? userId, String? email, String? name}){
+    return MyUser(userId: userId ?? this.userId, email: email ?? this.email,name: name?? this.name);
+  }
+
+  MyUserEntity toEntity(){
+    return MyUserEntity (userId: userId, email: email, name: name);
+  }
+  
+  static MyUser fromEntity(MyUserEntity entity){
+    return MyUser(userId: entity.userId, email: entity.email, name: entity.name);
+  }
+
+  static const empty=MyUser(userId: '', email: '', name: '');
+
+  @override
+  List<Object?> get props => [userId, email, name];
+
+}
