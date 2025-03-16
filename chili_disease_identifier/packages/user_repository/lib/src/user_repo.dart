20acb9027cebbac3ef5d2c7901980f81036e6b15
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2025-03-16 19:28:03 +0530

    feat:app authentication

--- chili_disease_identifier/packages/user_repository/lib/src/user_repo.dart
+++ chili_disease_identifier/packages/user_repository/lib/src/user_repo.dart
@@ -6,4 +6,5 @@ abstract class UserRepository{
   Future<MyUser>signUp(MyUser myUser,String password);
   Future<void>setUserData(MyUser user);
   Future<void>signIn(String email,String password);
+  Future<void> logOut();
 }
